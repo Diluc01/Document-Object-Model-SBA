253c1ee742cf29c6b498d9f6b028566ebd12b578
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-19 11:25:11 -0400

    working on styling forms

--- index.js
+++ index.js
@@ -22,13 +22,17 @@ list.appendChild(item3);
 list.appendChild(item4);
 
 let main = document.querySelector(".main");
+main.classList.add("form-display");
 main.style.backgroundColor = "green";
 main.style.height = "100vh";
+main.style.padding = "16px";
 let registrationForm = document.createElement("form");
 registrationForm.classList.add("main-display");
 registrationForm.style.height = "200px";
 let loginForm = document.createElement("form");
+loginForm.classList.add("main-display");
 main.appendChild(registrationForm);
+main.appendChild(loginForm);
 
 let labelsAndInputs = [
   {
@@ -68,8 +72,17 @@ let labelsAndInputs = [
 labelsAndInputs.forEach((label) => {
   let label2 = document.createElement("label");
   label2.setAttribute("for", label.for);
+  label2.style.fontSize = "24px";
   label2.textContent = label.text;
+  label2.style.fontFamily = "Calibri";
   let input2 = document.createElement("input");
+  input2.style.padding = "10px";
+  input2.style.display = "inline-block";
+  input2.style.border = "none";
+  input2.style.borderRadius = "4px";
+  input2.style.backgroundColor = "#f1f1f1";
+  input2.style.fontSize = "24px";
+  input2.style.marginBottom = "15px";
   input2.setAttribute("type", label.type);
   input2.setAttribute("placeholder", label.placeholder);
   input2.setAttribute("name", label.name);
@@ -78,3 +91,48 @@ labelsAndInputs.forEach((label) => {
   registrationForm.appendChild(label2);
   registrationForm.appendChild(input2);
 });
+
+let button = document.createElement("button");
+button.setAttribute("type", "submit");
+button.setAttribute("class", "registerbtn");
+button.textContent = "Sign up";
+registrationForm.appendChild(button);
+
+let labelsAndInputs2 = [
+  {
+    text: "Username",
+    for: "username",
+    type: "text",
+    placeholder: "Username",
+    name: "username",
+    id: "username",
+  },
+  {
+    text: "Password",
+    for: "psw2",
+    type: "password",
+    placeholder: "Password",
+    name: "password2",
+    id: "psw2",
+  },
+];
+
+labelsAndInputs2.forEach((label) => {
+  let label2 = document.createElement("label");
+  label2.setAttribute("for", label.for);
+  label2.textContent = label.text;
+  let input2 = document.createElement("input");
+  input2.setAttribute("type", label.type);
+  input2.setAttribute("placeholder", label.placeholder);
+  input2.setAttribute("name", label.name);
+  input2.setAttribute("id", label.id);
+
+  loginForm.appendChild(label2);
+  loginForm.appendChild(input2);
+});
+
+let button2 = document.createElement("button");
+button2.setAttribute("type", "submit");
+button2.setAttribute("class", "LogInBtn");
+button2.textContent = "Log in";
+loginForm.appendChild(button2);
